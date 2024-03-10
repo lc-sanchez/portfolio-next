dac5120d65bdc92d8c2dbbf96a16a2435d560a49
lc-sanchez <csanchez_98@outlook.com>
2024-03-10 20:26:00 -0300

    Cambiando contactame por redes

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -34,7 +34,9 @@ export const HeroSection = () => {
                     <button className='px-6 py-3 w-full 
                     sm:w-fit rounded-full mr-4 
                     bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-gradient-to-br hover:from-pink-500 hover:via-purple-500 hover:to-blue-500 text-white font-mono'>
-                        <Link href="#contactame">Contáctame</Link>
+                        <Link href="#contactame">
+                            Redes
+                        </Link>
                     </button>
                     <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-gradient-to-br  hover:from-pink-500 hover:via-purple-500 hover:to-blue-500 text-white mt-3'>
                         <a download href="./CV.pdf" alt="Mi CV" className='block hover:bg-slate-900 rounded-full px-5 py-2 font-mono' style={{
